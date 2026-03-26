18540d3b5c5fa8e1fbfccc450302f646048c1aeb
Bisan Garba <your_github_email@example.com>
2026-03-26 11:40:49 +0100

    add bat favicon and app icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,25 @@ export const metadata: Metadata = {
   title: "NigStar",
   description:
     "Modern Social Media Application for blacks starting from Nigeria, Powered by NextJs",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    other: [
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
